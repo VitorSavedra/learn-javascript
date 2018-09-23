7dd2b27d3468812b79defafb3838bdae1aaa18d8
Vitor Savedra <vitor-savedra@outlook.com>
2018-09-23 20:07:28 -0300

    Limpeza de código

--- advanced-javascript-i/client/js/app/controllers/NegociacaoController.js
+++ advanced-javascript-i/client/js/app/controllers/NegociacaoController.js
@@ -1,24 +1,18 @@
 class NegociacaoController {
-
   constructor() {
-
     let $ = document.querySelector.bind(document);
-    this._inputData = $('#data');
-    this._inputQuantidade = $('#quantidade');
-    this._inputValor = $('#valor');
+    this._inputData = $("#data");
+    this._inputQuantidade = $("#quantidade");
+    this._inputValor = $("#valor");
   }
 
   adiciona(event) {
-
     event.preventDefault();
 
-    let negociacao = new Negociacao (
+    let negociacao = new Negociacao(
       DateHelper.textoParaData(this._inputData.value),
       this._inputQuantidade.value,
       this._inputValor.value
     );
-
-    console.log(negociacao);
-    console.log(DateHelper.dataParaTexto(negociacao.data));
   }
 }
