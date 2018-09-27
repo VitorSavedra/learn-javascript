869b4d1df8ab8c8966c55ead5af1de7892b66eeb
Vitor Savedra <vitor-savedra@outlook.com>
2018-09-26 22:45:12 -0300

    Abstração de renderização de tabela

--- advanced-javascript-i/client/js/app/controllers/NegociacaoController.js
+++ advanced-javascript-i/client/js/app/controllers/NegociacaoController.js
@@ -5,11 +5,12 @@ class NegociacaoController {
     this._inputQuantidade = $("#quantidade");
     this._inputValor = $("#valor");
     this._listaNegociacoes = new ListaNegociacoes();
+    this._negociacoesView = new NegociacoesView($("#negociacoesView"));
+    this._negociacoesView.update(this._listaNegociacoes);
   }
 
   adiciona(event) {
     event.preventDefault();
-
     this._listaNegociacoes.adiciona(this._criaNegociacao());
     this._limpaFormulario();
   }
@@ -26,7 +27,6 @@ class NegociacaoController {
     this._inputData.value = "";
     this._inputQuantidade.value = 1;
     this._inputValor.value = 0.0;
-
     this._inputData.focus();
   }
 }
